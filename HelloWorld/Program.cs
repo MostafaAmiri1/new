220367ad15041481109462bea6cb47526e8c075e
mostafa amiri <amiri@MacBook-Air-4.local>
2024-01-11 11:24:22 +0100

    en liten endring

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("her er en fizzbuzz kode");
+
+Console.WriteLine(" Fizz Buzz");
+Console.WriteLine("----------");
+
+for (int i = 1; i <= 100; i++)
+{
+    Console.WriteLine(i);
+    if (i % 3 == 0)
+    {
+        Console.Write("Fizz");
+    }
+    
+    if (i % 5 == 0)
+    {
+        Console.Write("Buzz");
+    } 
+}
+
